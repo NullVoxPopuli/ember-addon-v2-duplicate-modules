c196a2927b3b958f83999155ba99cd1d421ee0f1
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-02-12 09:21:58 -0500

    add setupTest

--- testing/ember-app/tests/_the_-test.js
+++ testing/ember-app/tests/_the_-test.js
@@ -1,8 +1,11 @@
 import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
 
 import { initialize } from 'my-addon/instance-initializers/test-initializer';
 
-module('The Test', function () {
+module('The Test', function (hooks) {
+  setupTest(hooks);
+
   test('did initializer laod?', function (assert) {
     assert.strictEqual(window.i, 1);
 
